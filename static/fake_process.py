13c70a474be7c0c32e43397037f8f85422ef115b
Coombszy <LCCoombs@hotmail.co.uk>
2022-10-14 15:22:09 +0100

    Rename var

--- static/fake_process.py
+++ static/fake_process.py
@@ -1,15 +1,15 @@
 # Title: Fake process
-# Description: Fake script that fakes execution taking {TIME} amount of time
+# Description: Fake script that fakes execution taking {FAKE_TIME} amount of time
 # Version: 99.99.99
 # Arguments: 
-#   - TIME
+#   - FAKE_TIME
 ##########################################################################################
 # Using shared lib, import and validate arguments
-list = ["TIME"] 
+list = ["FAKE_TIME"] 
 from lib.args import args
 args = args(list)
 ##########################################################################################
 import time
-time.sleep(int(args["TIME"]))
+time.sleep(int(args["FAKE_TIME"]))
 print("Wait finished!")
 exit(0)
